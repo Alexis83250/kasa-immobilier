adb7882579db1ad38d63c81c5de9d0fb39be8470
Alexis83250 <alexis.gallien83@gmail.com>
2023-08-15 15:47:48 +0200

    propos fait

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import erreur from '../../assets/erreur.png'
 import '../../styles/Error.scss'
 import '../../styles/Body.scss'
@@ -7,8 +8,11 @@ import '../../styles/Body.scss'
 function Error() {
     return (
         <div className='error'>
-            <img src={erreur} alt="message d'erreur" />
-            <h1>Oups ! La page que vous demandez n'existe pas</h1>
+            <img className="padding" src={erreur} alt="message d'erreur" />
+            <h1 className="error__color padding">Oups ! La page que vous demandez n'existe pas</h1>
+            <Link to="/">
+                <p className="error__color padding ">Retourner sur la page d'acceuil</p> 
+        </Link>
         </div>
     )
 }
--- src/pages/Propos/index.jsx
+++ src/pages/Propos/index.jsx
@@ -1,11 +1,18 @@
 import ImageFond2 from '../../assets/ImageFond2.png'
+import Fleche from '../../assets/fleche-haute.png'
 import '../../styles/Banner.scss'
+import '../../styles/Propos.scss'
 
 
 function Propos() {
-  return (
+  return (<div className='propos ajustement'>
     <div className='Banniere'>
       <img className="Banniere__ImageFond" src={ImageFond2} alt='Paysage en fond'/>
+    </div>
+    <div className='ajustement propos__closed'><p>Fiabilité</p><img className='propos__closed--logo' src={Fleche} alt='Fleche pour dérouler'/></div>
+    <div className='ajustement propos__closed'><p>Respect</p><img className='propos__closed--logo' src={Fleche} alt='Fleche pour dérouler'/></div>
+    <div className='ajustement propos__closed'><p>Service</p><img className='propos__closed--logo' src={Fleche} alt='Fleche pour dérouler'/></div>
+    <div className='ajustement propos__closed'><p>Sécurité</p><img className='propos__closed--logo' src={Fleche} alt='Fleche pour dérouler'/></div>
     </div>
   )
 }
